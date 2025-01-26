673984d936fa092260d02af55e8b9cd6ab8bc2e5
Zoha Khan <zohakhan11004@gmail.com>
2025-01-26 10:10:50 +0000

    fetch-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CreateData from "./components/create-data/create-data"
 import Home from "./components/home/home"
 
 const App = () => {
   return (
     <div>
       <Home/>
+      <CreateData/>
     </div>
   )
 }
